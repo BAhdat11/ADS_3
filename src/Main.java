5f1bccea5a8ae124c69224a98c7c789950f15fed
BAhdat11 <ayaganbagdat1@icloud.com>
2025-04-29 01:38:47 +0500

    d

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        MyHashTable<MyTestingClass, Integer> table = new MyHashTable<>();
+        Random random = new Random();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i < 10000; i++) {
+            int id = random.nextInt(10000);
+            String name = "Test" + random.nextInt(10000);
+            table.put(new MyTestingClass(id, name), i);
+        }
+
+        for (int i = 0; i < 11; i++) {
+            int count = 0;
+            var node = table.getChain(i);
+            while (node != null) {
+                count++;
+                node = node.next;
+            }
+            System.out.println("Bucket " + i + " has " + count + " elements");
         }
     }
 }
